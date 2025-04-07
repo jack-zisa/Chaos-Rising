e650784d0412b05bc4788f220ce36214895d95c5
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-04-07 09:11:06 -0400

    Remove all python files

--- render/camera.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class Camera:
-    def __init__(self, width: int, height: int):
-        self.pos = pygame.Vector2(0, 0)
-        self.size = pygame.Vector2(width, height)
--- util/vec_utils.py
+++
@@ -1,3 +0,0 @@
-import pygame
-
-ZERO: pygame.Vector2 = pygame.Vector2(0, 0)
