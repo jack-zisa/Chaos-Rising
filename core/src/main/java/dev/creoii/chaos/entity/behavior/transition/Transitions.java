1a94290b6dc96f965eb92a77a46eb3e35db49643
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-04-10 01:08:42 -0400

    "previous" transition
    Allow "to" transition to accept an int/string

--- core/src/main/java/dev/creoii/chaos/entity/behavior/transition/Transitions.java
+++ core/src/main/java/dev/creoii/chaos/entity/behavior/transition/Transitions.java
@@ -1,5 +1,7 @@
 package dev.creoii.chaos.entity.behavior.transition;
 
+import com.badlogic.gdx.utils.JsonValue;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,13 +9,15 @@ public class Transitions {
     public static final Map<String, Transition> ALL = new HashMap<>();
 
     static {
-        Transition.register("next", (controller, phase, data) -> {
-            int index = (controller.getIndex(phase) + 1) % controller.getPhaseCount();
-            return controller.getPhase(index);
-        });
-
+        Transition.register("next", (controller, phase, data) -> controller.getPhase((controller.getIndex(phase) + 1) % controller.getPhaseCount()));
+        Transition.register("previous", (controller, phase, data) -> controller.getPhase((controller.getIndex(phase) + - 1) % controller.getPhaseCount()));
         Transition.register("random", (controller, phase, data) -> controller.getPhase(controller.getRandom().nextInt(controller.getPhaseCount())));
-
-        Transition.register("to", (controller, phase, data) -> controller.getPhase(data.getInt("to")));
+        Transition.register("to", (controller, phase, data) -> {
+            JsonValue toValue = data.get("to");
+            if (toValue.isNumber()) {
+                return controller.getPhase(data.getInt("to"));
+            }
+            return controller.getPhase(data.getString("to"));
+        });
     }
 }
