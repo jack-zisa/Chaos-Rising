fc0194e07ff5325884d084379eea611ef2f33151
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-10-28 01:27:59 -0400

    Fix movement actions

--- core/src/main/java/dev/creoii/chaos/entity/behavior/action/Movements.java
+++ core/src/main/java/dev/creoii/chaos/entity/behavior/action/Movements.java
@@ -1,8 +1,15 @@
 package dev.creoii.chaos.entity.behavior.action;
 
+import com.badlogic.gdx.math.Vector2;
+import dev.creoii.chaos.EntityManager;
+import dev.creoii.chaos.Game;
+import dev.creoii.chaos.entity.CharacterEntity;
 import dev.creoii.chaos.entity.Entity;
+import dev.creoii.chaos.entity.LivingEntity;
+import dev.creoii.chaos.util.EntityGroup;
 
-import java.util.Map;
+import javax.annotation.Nullable;
+import java.util.*;
 import java.util.function.BiConsumer;
 
 public class Movements {
@@ -12,14 +19,30 @@ public class Movements {
     );
 
     public static void random(Entity entity, float dt) {
-        //float speed = (entity instanceof LivingEntity living ? living.getStats().speed().value() : 1f) * data.getFloat("speed", 1f);
-        //entity.getPos().x += (Math.random() < .5f ? -1f : 1f) * speed * Entity.COORDINATE_SCALE * dt;
-        //entity.getPos().y += (Math.random() < .5f ? -1f : 1f) * speed * Entity.COORDINATE_SCALE * dt;
+        float speed = (entity instanceof LivingEntity living ? living.getStats().speed().value() : 1f);
+        entity.getPos().x += (Math.random() < .5f ? -1f : 1f) * speed * dt;
+        entity.getPos().y += (Math.random() < .5f ? -1f : 1f) * speed * dt;
     }
 
     public static void chase(Entity entity, float dt) {
-        /*float speed = (entity instanceof LivingEntity living ? living.getStats().speed.value() : 1f) * data.getFloat("speed", 1f);
-        Vector2 direction = new Vector2(entity.getGame().getActiveCharacter().getCenterPos()).sub(entity.getCenterPos()).nor();
-        entity.getPos().add(direction.nor().scl(speed * Entity.COORDINATE_SCALE * dt));*/
+        CharacterEntity character = getNearestCharacter(entity);
+        if (character != null) {
+            float speed = (entity instanceof LivingEntity living ? living.getStats().speed().value() : 1f);
+            Vector2 direction = new Vector2(character.getPos()).sub(entity.getPos()).nor();
+            entity.getPos().add(direction.nor().scl(speed * dt));
+        }
+    }
+
+    @Nullable
+    public static CharacterEntity getNearestCharacter(Entity entity) {
+        Game game = entity.getGame();
+        if (!game.isClient()) {
+            EntityManager<?> entityManager = game.getEntityManager();
+            return entityManager.getEntities(EntityGroup.CHARACTER).values().stream()
+                .map(o -> (CharacterEntity) o)
+                .min(Comparator.comparingDouble(c -> entity.getPos().dst2(c.getPos())))
+                .orElse(null);
+        }
+        return null;
     }
 }
