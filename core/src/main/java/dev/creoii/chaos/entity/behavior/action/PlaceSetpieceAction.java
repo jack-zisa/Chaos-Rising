8b72458911a23ae9a61db10123e6c4e81d070f2a
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-02-02 22:43:23 -0500

    Add offset parameter

--- core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceSetpieceAction.java
+++ core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceSetpieceAction.java
@@ -1,5 +1,6 @@
 package dev.creoii.chaos.entity.behavior.action;
 
+import com.badlogic.gdx.math.Vector2;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.creoii.chaos.DataManager;
@@ -8,21 +9,29 @@ import dev.creoii.chaos.entity.Entity;
 import dev.creoii.chaos.entity.controller.EntityController;
 import dev.creoii.chaos.util.context.Context;
 import dev.creoii.chaos.util.provider.stringprovider.StringProvider;
+import dev.creoii.chaos.util.provider.vecprovider.VecProvider;
 
 public class PlaceSetpieceAction extends InstantAction {
     public static final MapCodec<PlaceSetpieceAction> CODEC = RecordCodecBuilder.mapCodec(instance -> {
         return instance.group(
-            StringProvider.CODEC.fieldOf("setpiece").forGetter(PlaceSetpieceAction::getSetpiece)
+            StringProvider.CODEC.fieldOf("setpiece").forGetter(PlaceSetpieceAction::getSetpiece),
+            VecProvider.CODEC.fieldOf("offset").forGetter(PlaceSetpieceAction::getOffset)
         ).apply(instance, PlaceSetpieceAction::new);
     });
     private final StringProvider setpiece;
+    private final VecProvider offset;
 
-    public PlaceSetpieceAction(StringProvider setpiece) {
+    public PlaceSetpieceAction(StringProvider setpiece, VecProvider offset) {
             this.setpiece = setpiece;
+        this.offset = offset;
     }
 
     public StringProvider getSetpiece() {
         return setpiece;
+    }
+
+    public VecProvider getOffset() {
+        return offset;
     }
 
     @Override
@@ -32,8 +41,11 @@ public class PlaceSetpieceAction extends InstantAction {
 
     @Override
     public void start(EntityController<? extends EnemyEntity> controller) {
-        int x = Math.round(controller.getEntity().getPos().x / Entity.COORDINATE_SCALE);
-        int y = Math.round(controller.getEntity().getPos().y / Entity.COORDINATE_SCALE);
-        controller.getEntity().getWorld().placeSetpiece(DataManager.getSetpiece(setpiece.get(Context.rootOf(controller.getEntity()))), x, y);
+        EnemyEntity entity = controller.getEntity();
+        Context context = Context.rootOf(entity);
+        int x = Math.round(entity.getPos().x / Entity.COORDINATE_SCALE);
+        int y = Math.round(entity.getPos().y / Entity.COORDINATE_SCALE);
+        Vector2 offset = this.offset.get(context);
+        entity.getWorld().placeSetpiece(DataManager.getSetpiece(setpiece.get(context)), x + (int) offset.x, y + (int) offset.y);
     }
 }
--- core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceTileAction.java
+++ core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceTileAction.java
@@ -1,5 +1,6 @@
 package dev.creoii.chaos.entity.behavior.action;
 
+import com.badlogic.gdx.math.Vector2;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.creoii.chaos.entity.EnemyEntity;
@@ -7,21 +8,29 @@ import dev.creoii.chaos.entity.Entity;
 import dev.creoii.chaos.entity.controller.EntityController;
 import dev.creoii.chaos.util.context.Context;
 import dev.creoii.chaos.util.provider.tileprovider.TileProvider;
+import dev.creoii.chaos.util.provider.vecprovider.VecProvider;
 
 public class PlaceTileAction extends InstantAction {
     public static final MapCodec<PlaceTileAction> CODEC = RecordCodecBuilder.mapCodec(instance -> {
         return instance.group(
-            TileProvider.CODEC.fieldOf("tile").forGetter(PlaceTileAction::getTile)
+            TileProvider.CODEC.fieldOf("tile").forGetter(PlaceTileAction::getTile),
+            VecProvider.CODEC.fieldOf("offset").forGetter(PlaceTileAction::getOffset)
         ).apply(instance, PlaceTileAction::new);
     });
     private final TileProvider tile;
+    private final VecProvider offset;
 
-    public PlaceTileAction(TileProvider tile) {
+    public PlaceTileAction(TileProvider tile, VecProvider offset) {
         this.tile = tile;
+        this.offset = offset;
     }
 
     public TileProvider getTile() {
         return tile;
+    }
+
+    public VecProvider getOffset() {
+        return offset;
     }
 
     @Override
@@ -31,8 +40,11 @@ public class PlaceTileAction extends InstantAction {
 
     @Override
     public void start(EntityController<? extends EnemyEntity> controller) {
-        int x = Math.round(controller.getEntity().getPos().x / Entity.COORDINATE_SCALE);
-        int y = Math.round(controller.getEntity().getPos().y / Entity.COORDINATE_SCALE);
-        controller.getEntity().getWorld().setGround(x, y, tile.get(Context.rootOf(controller.getEntity())));
+        EnemyEntity entity = controller.getEntity();
+        Context context = Context.rootOf(entity);
+        int x = Math.round(entity.getPos().x / Entity.COORDINATE_SCALE);
+        int y = Math.round(entity.getPos().y / Entity.COORDINATE_SCALE);
+        Vector2 offset = this.offset.get(context);
+        entity.getWorld().setGround(x + (int) offset.x, y + (int) offset.y, tile.get(context));
     }
 }
