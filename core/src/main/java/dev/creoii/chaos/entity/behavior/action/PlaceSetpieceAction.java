7da3b2f8e2b738c0f01b74622c4d045066f89269
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-02-02 22:38:33 -0500

    Update PlaceSetpieceAction.java

--- core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceSetpieceAction.java
+++ core/src/main/java/dev/creoii/chaos/entity/behavior/action/PlaceSetpieceAction.java
@@ -1,26 +1,27 @@
 package dev.creoii.chaos.entity.behavior.action;
 
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.creoii.chaos.DataManager;
 import dev.creoii.chaos.entity.EnemyEntity;
 import dev.creoii.chaos.entity.Entity;
 import dev.creoii.chaos.entity.controller.EntityController;
+import dev.creoii.chaos.util.context.Context;
+import dev.creoii.chaos.util.provider.stringprovider.StringProvider;
 
 public class PlaceSetpieceAction extends InstantAction {
     public static final MapCodec<PlaceSetpieceAction> CODEC = RecordCodecBuilder.mapCodec(instance -> {
         return instance.group(
-            Codec.STRING.fieldOf("setpiece").forGetter(PlaceSetpieceAction::getSetpiece)
+            StringProvider.CODEC.fieldOf("setpiece").forGetter(PlaceSetpieceAction::getSetpiece)
         ).apply(instance, PlaceSetpieceAction::new);
     });
-    private final String setpiece;
+    private final StringProvider setpiece;
 
-    public PlaceSetpieceAction(String setpiece) {
+    public PlaceSetpieceAction(StringProvider setpiece) {
             this.setpiece = setpiece;
     }
 
-    public String getSetpiece() {
+    public StringProvider getSetpiece() {
         return setpiece;
     }
 
@@ -33,6 +34,6 @@ public class PlaceSetpieceAction extends InstantAction {
     public void start(EntityController<? extends EnemyEntity> controller) {
         int x = Math.round(controller.getEntity().getPos().x / Entity.COORDINATE_SCALE);
         int y = Math.round(controller.getEntity().getPos().y / Entity.COORDINATE_SCALE);
-        controller.getEntity().getWorld().placeSetpiece(DataManager.getSetpiece(setpiece), x, y);
+        controller.getEntity().getWorld().placeSetpiece(DataManager.getSetpiece(setpiece.get(Context.rootOf(controller.getEntity()))), x, y);
     }
 }
